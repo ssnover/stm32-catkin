ae7a20f6a19ec303150b0a686643bebb7f4ae4b9
ssnover95 <sws7379@rit.edu>
2019-03-01 21:24:52 -0700

    Initial commit of two package, one driver and one blink package

---
+++ app-blink/src/main.cpp
@@ -0,0 +1,197 @@
+/*
+ * file: main.cpp
+ */
+
+#include <stm32f4xx_hal_cortex.h>
+#include <stm32f4xx_hal_gpio.h>
+#include <stm32f4xx_hal_rcc.h>
+#include <core_cm4.h>
+#include <stm32f446xx.h>
+#include <stm32f4xx_hal_pwr_ex.h>
+#include <stm32f4xx_hal_flash_ex.h>
+#include "stm32f4xx_hal.h"
+#include "stm32f4xx_hal_uart.h"
+
+UART_HandleTypeDef huart1;
+
+void _Error_Handler();
+
+void SystemClock_Config();
+static void MX_GPIO_Init();
+static void MX_USART1_UART_Init();
+
+#define NOP() __asm volatile(" nop\r\n")
+
+/**
+ * @brief This function handles Hard fault interrupt.
+ */
+extern "C"
+{
+void HardFault_Handler()
+{
+    while (true)
+    {
+        NOP();
+    }
+}
+
+void SysTick_Handler(void)
+{
+    HAL_IncTick();
+    HAL_SYSTICK_IRQHandler();
+}
+
+void UsageFault_Handler(void)
+{
+    while (true)
+    {
+        NOP();
+    }
+}
+
+void WWDG_IRQHandler()
+{
+    while (true)
+    {
+        NOP();
+    }
+}
+}
+
+/**
+ * @brief  The application entry point.
+ *
+ * @retval None
+ */
+int main()
+{
+    HAL_Init();
+
+    /* Configure the system clock */
+    SystemClock_Config();
+
+    /* Initialize all configured peripherals */
+    MX_GPIO_Init();
+    MX_USART1_UART_Init();
+
+    /* Infinite loop */
+    while (true)
+    {
+        HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
+        for (auto i = 0u; i < 1000000u; ++i)
+        {
+            NOP();
+        }
+    }
+}
+
+/**
+ * @brief System Clock Configuration
+ * @retval None
+ */
+void SystemClock_Config()
+{
+    RCC_OscInitTypeDef RCC_OscInitStruct;
+    RCC_ClkInitTypeDef RCC_ClkInitStruct;
+
+    /**Configure the main internal regulator output voltage
+     */
+    __HAL_RCC_PWR_CLK_ENABLE();
+
+    __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE3);
+
+    /**Initializes the CPU, AHB and APB busses clocks
+     */
+    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+    RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+    RCC_OscInitStruct.HSICalibrationValue = 16;
+    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+    RCC_OscInitStruct.PLL.PLLM = 16;
+    RCC_OscInitStruct.PLL.PLLN = 192;
+    RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+    RCC_OscInitStruct.PLL.PLLQ = 2;
+    RCC_OscInitStruct.PLL.PLLR = 2;
+    if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+    {
+        _Error_Handler();
+    }
+
+    /**Initializes the CPU, AHB and APB busses clocks
+     */
+    RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK |
+                                  RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+
+    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_3) != HAL_OK)
+    {
+        _Error_Handler();
+    }
+
+    /**Configure the Systick interrupt time
+     */
+    HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq() / 1000);
+
+    /**Configure the Systick
+     */
+    HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
+
+    /* SysTick_IRQn interrupt configuration */
+    HAL_NVIC_SetPriority(SysTick_IRQn, 0, 0);
+}
+
+/* USART1 init function */
+static void MX_USART1_UART_Init()
+{
+    huart1.Instance = USART1;
+    huart1.Init.BaudRate = 19200;
+    huart1.Init.WordLength = UART_WORDLENGTH_8B;
+    huart1.Init.StopBits = UART_STOPBITS_1;
+    huart1.Init.Parity = UART_PARITY_NONE;
+    huart1.Init.Mode = UART_MODE_TX_RX;
+    huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+    huart1.Init.OverSampling = UART_OVERSAMPLING_16;
+    if (HAL_UART_Init(&huart1) != HAL_OK)
+    {
+        _Error_Handler();
+    }
+}
+
+/** Configure pins as
+ * Analog
+ * Input
+ * Output
+ * EVENT_OUT
+ * EXTI
+ */
+static void MX_GPIO_Init()
+{
+    GPIO_InitTypeDef GPIO_InitStruct;
+
+    /* GPIO Ports Clock Enable */
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+
+    /*Configure GPIO pin Output Level */
+    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_RESET);
+
+    /*Configure GPIO pin : PA5 */
+    GPIO_InitStruct.Pin = GPIO_PIN_5;
+    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+}
+
+/**
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
+void _Error_Handler()
+{
+    while (true)
+    {
+    }
+}
---
+++ app-blink/toolchain/stm32f4-toolchain.cmake
@@ -0,0 +1,24 @@
+set(CMAKE_SYSTEM_NAME Generic)
+set(CMAKE_SYSTEM_VERSION 1)
+cmake_minimum_required(VERSION 3.7)
+
+# specify the cross compiler
+set(CMAKE_C_COMPILER_WORKS 1)
+set(CMAKE_C_COMPILER /home/ssnover/tools/gcc-arm-none-eabi-7-2018-q2-update/bin/arm-none-eabi-gcc)
+set(CMAKE_CXX_COMPILER_WORKS 1)
+set(CMAKE_CXX_COMPILER /home/ssnover/tools/gcc-arm-none-eabi-7-2018-q2-update/bin/arm-none-eabi-g++)
+set(AS /home/ssnover/tools/gcc-arm-none-eabi-7-2018-q2-update/bin/arm-none-eabi-as)
+set(AR arm-none-eabi-ar)
+set(OBJCOPY arm-none-eabi-objcopy)
+set(OBJDUMP arm-none-eabi-objdump)
+set(SIZE arm-none-eabi-size)
+
+add_definitions(
+    -DSTM32F446xx
+    -DUSE_HAL_DRIVER
+    -DARM_MATH_CM4
+    -DARM_MATH_MATRIX_CHECK
+    -DARM_MATH_ROUNDING
+    -D__weak=__attribute__\(\(weak\)\)
+    -D__packed=__attriubte__\(\(__packed__\)\)
+)
